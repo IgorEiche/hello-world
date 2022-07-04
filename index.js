9289926c22c9ae7d4d49d2ddcfa25fa515c00d2b
Igor Carvalho <igorribcarvalho@gmail.com>
2022-07-04 19:19:49 -0300

    alterações da branch feature foram acrecidas à develop

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* Professores, fiquei em dúvida se o console.log('feature') deveria substituir a mensagem exibida por console.log('git flow') em index.js
+/* Professores, fiquei em dúvida se o console.log('feature') do branch 'feature' deveria substituir a mensagem exibida por console.log('git flow') em index.js
 ou se deveria substituir aquela que fora exibida por console.log("hello world") em hello-world.js
 optei pelo primeiro caso.  */
 console.log("feature");
