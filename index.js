5215c9a7cacb6ba42a972088316579667b3ef2c0
Igor Carvalho <igorribcarvalho@gmail.com>
2022-07-04 19:10:27 -0300

    'feature' branch, exibindo a mensagem 'feature' em index.js, adicionado ao branch 'develop'

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("git flow");
+/* Professores, fiquei em dúvida se o console.log('feature') deveria substituir a mensagem exibida por console.log('git flow') em index.js
+ou se deveria substituir aquela que fora exibida por console.log("hello world") em hello-world.js
+optei pelo primeiro caso.  */
+console.log("feature");
